edce4e2db6e4ecc5bedbcd20faeeaa38b564ed70
Andrii Odintsov <andrii.odintsov@dvigunity.com>
2026-01-06 19:56:28 +0200

    catalog categories

--- src/main/java/com/odintsov/wallpapers_project/presentation/controllers/CatalogController.java
+++ src/main/java/com/odintsov/wallpapers_project/presentation/controllers/CatalogController.java
@@ -1,12 +1,17 @@
 package com.odintsov.wallpapers_project.presentation.controllers;
 
+import com.odintsov.wallpapers_project.application.dtos.CategoryResponse;
+import com.odintsov.wallpapers_project.application.mappers.CategoryMapper;
 import com.odintsov.wallpapers_project.application.usecases.GlobalSearch.GlobalSearchResponse;
 import com.odintsov.wallpapers_project.application.usecases.GlobalSearch.GlobalSearchUseCase;
+import com.odintsov.wallpapers_project.domain.repositories.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/catalog")
@@ -14,9 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class CatalogController {
 
     private final GlobalSearchUseCase globalSearchUseCase;
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
 
     @GetMapping("/search")
     public GlobalSearchResponse search(@RequestParam String query) {
         return globalSearchUseCase.execute(query);
+    }
+
+    @GetMapping("/categories")
+    public List<CategoryResponse> getCategories() {
+        return categoryRepository.findAll().stream()
+                .map(categoryMapper::toResponse)
+                .toList();
     }
 }
